7bfa7f501f01a8aa37f608ddc49c74ae15fb72c4
Mark Canlas <github@htmlism.com>
2024-08-19 13:39:04 -0400

    scala 3.5.0

--- project/CrossPublish2And3Plugin.scala
+++ project/CrossPublish2And3Plugin.scala
@@ -12,6 +12,6 @@ object CrossPublish2And3Plugin extends AutoPlugin {
   override def trigger: PluginTrigger = AllRequirements
 
   override val buildSettings: Seq[Setting[?]] = Seq(
-    crossScalaVersions := Seq("2.13.14", "3.4.2")
+    crossScalaVersions := Seq("2.13.14", "3.5.0")
   )
 }

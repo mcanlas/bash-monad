c051623d6c4c467a0a90526afd402e9f299e6725
Mark Canlas <github@htmlism.com>
2026-03-13 21:17:48 -0400

    dependencies :zap:

---
+++ project/DependenciesPlugin.scala
@@ -0,0 +1,26 @@
+import sbt.Keys.*
+import sbt.*
+
+object DependenciesPlugin extends AutoPlugin {
+  override def trigger = allRequirements
+
+  object autoImport {
+    implicit class DependencyOps(p: Project) {
+      def withCats: Project =
+        p
+          .settings(libraryDependencies += "org.typelevel" %% "cats-core" % "2.13.0")
+
+      def withTesting: Project = {
+        val weaverVersion =
+          "0.8.4"
+
+        p.settings(
+          libraryDependencies ++= Seq(
+            "com.disneystreaming" %% "weaver-cats"       % weaverVersion % Test,
+            "com.disneystreaming" %% "weaver-scalacheck" % weaverVersion % Test
+          )
+        )
+      }
+    }
+  }
+}
--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -23,23 +23,5 @@ object ProjectPlugin extends AutoPlugin {
     def module(s: String): Project =
       Project(s, file(jarName(s)))
         .settings(name := jarName(s))
-
-    implicit class ProjectOps(p: Project) {
-      def withCats: Project =
-        p
-          .settings(libraryDependencies += "org.typelevel" %% "cats-core" % "2.13.0")
-
-      def withTesting: Project = {
-        val weaverVersion =
-          "0.8.4"
-
-        p.settings(
-          libraryDependencies ++= Seq(
-            "com.disneystreaming" %% "weaver-cats"       % weaverVersion % Test,
-            "com.disneystreaming" %% "weaver-scalacheck" % weaverVersion % Test
-          )
-        )
-      }
-    }
   }
 }
